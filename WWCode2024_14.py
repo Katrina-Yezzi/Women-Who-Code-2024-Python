622a3c55d3b94d560b5756c9a918f251b1218d67
Katrina Yezzi-Woodley <97074259+Katrina-Yezzi@users.noreply.github.com>
2024-01-24 10:14:30 -0600

    Update WWCode2024_14.py
    Corrected for F(0) and F(1) etc.

--- WWCode2024_14.py
+++ WWCode2024_14.py
@@ -1,17 +1,22 @@
 # Women Who Code (Python) Challenge No. 14
 # Write a program to print the first n numbers of a Fibonacci sequence.
 
+
 def fib_seq(n):
-    fib = [0,1]
+    fib = [1,1] 
 
-    for i in range(2,n):
-        next_num = fib[i-1] + fib[i-2]
-        fib.append(next_num)
+    if n == 0:
+        result=[0]
 
-    return fib[1:n]
+    else:
+        for i in range(2,n):           
+            next_num = fib[i-1] + fib[i-2]
+            fib.append(next_num)
+        result = fib[:n]
+            
+    print(f'F({n}) = {result}')
+        
+    return result
 
 n = int(input('Enter the value of n: '))
-result = fib_seq(n)
-
-print(f'First {n} numbers of Fibonacci sequence: {result}')
-        
+fib_seq(n)
